33604c57c4f398207c9de641c87886672a01482e
wilkin ruiz <wilkin.ruizvalerio@gmail.com>
2023-05-15 11:02:25 -0400

    get and getId 3rd Api

--- sample_service/main.py
+++ sample_service/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
+import requests
+from fastapi import APIRouter
 
 app = FastAPI()
+router = APIRouter()
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,3 +29,38 @@ def launch_details():
             "min": "00"
         }
     }
+
+
+@router.get('/recipes')
+async def search_recipes(query: str):
+    api_url = 'https://api.edamam.com/search'
+    api_key = '38cffb7fafa2f827a7426b8beb6970c3'
+    api_id = '7e9a5bb7'
+    params = {
+        'q': query,
+        'app_id': api_id,
+        'app_key': api_key,
+    }
+    response = requests.get(api_url, params=params)
+    data = response.json()
+    recipes = data.get('hits')
+    return {'recipes': recipes}
+
+
+@router.get('/recipes/{id}')
+async def get_recipe(id: str):
+    api_url = f'https://api.edamam.com/api/recipes/v2/{id}'
+    api_key = '38cffb7fafa2f827a7426b8beb6970c3'
+    api_id = '7e9a5bb7'
+    params = {
+        'type': 'public',
+        'app_id': api_id,
+        'app_key': api_key,
+    }
+    response = requests.get(api_url, params=params)
+    data = response.json()
+    recipe = data.get('recipe')
+    return {'recipe': recipe}
+
+
+app.include_router(router)
