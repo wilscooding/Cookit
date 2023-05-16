656bf15f114784fd4bdc71f864f2992d6c5bbebe
Caeden Lathem <camison7@gmail.com>
2023-05-16 12:47:22 -0400

    merged with nolan for users

---
+++ endpoints.py
@@ -0,0 +1,23 @@
+### Login
+
+# * Endpoint path: /login
+# * Endpoint method: POST
+# * Query parameters:
+#   * «name»: «purpose»
+
+# * Headers:
+#   * Authorization: Bearer token
+
+# * Request shape (JSON):
+#     ```json
+#     «JSON-looking thing that has the
+#     keys and types in it»
+#     ```
+
+# * Response: A login form
+
+# * Response shape (JSON):
+#     ```json
+#     «JSON-looking thing that has the
+#     keys and types in it»
+#     ```
