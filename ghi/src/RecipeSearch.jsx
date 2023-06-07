1c0d5ca3fdabf84d05ad400340fc44e2ac54c155
Caeden Lathem <camison7@gmail.com>
2023-06-07 17:37:18 -0400

    fixing class to className

--- ghi/src/RecipeSearch.jsx
+++ ghi/src/RecipeSearch.jsx
@@ -55,7 +55,7 @@ const RecipeSearch = ({ onRecipeSelect }) => {
 						<div role="status">
 							<svg
 								aria-hidden="true"
-								class="inline w-24 h-24 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-orange-400 dark:fill-gray-300"
+								className="inline w-24 h-24 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-orange-400 dark:fill-gray-300"
 								viewBox="0 0 100 101"
 								fill="none"
 								xmlns="http://www.w3.org/2000/svg"
@@ -69,7 +69,7 @@ const RecipeSearch = ({ onRecipeSelect }) => {
 									fill="currentFill"
 								/>
 							</svg>
-							<span class="sr-only">Loading...</span>
+							<span className="sr-only">Loading...</span>
 						</div>
 					</div>
 				</div>
@@ -95,7 +95,7 @@ const RecipeSearch = ({ onRecipeSelect }) => {
 										<div role="status">
 											<svg
 												aria-hidden="true"
-												class="inline w-24 h-24 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-orange-400 dark:fill-gray-300"
+												className="inline w-24 h-24 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-orange-400 dark:fill-gray-300"
 												viewBox="0 0 100 101"
 												fill="none"
 												xmlns="http://www.w3.org/2000/svg"
@@ -109,7 +109,7 @@ const RecipeSearch = ({ onRecipeSelect }) => {
 													fill="currentFill"
 												/>
 											</svg>
-											<span class="sr-only">Loading...</span>
+											<span className="sr-only">Loading...</span>
 										</div>
 									</div>
 								</div>
