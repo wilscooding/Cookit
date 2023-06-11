f8c2a873081070256b1470c6067e1c8381d79da5
wilkin ruiz <wilkin.ruizvalerio@gmail.com>
2023-06-11 16:44:57 -0400

    added hardcoded data to the grocery test and inventory test

--- cookit/tests/test_inventory.py
+++ cookit/tests/test_inventory.py
@@ -1,55 +1,92 @@
-# from fastapi.testclient import TestClient
-# from main import app
-# from db import MyIngredientIn
+from fastapi.testclient import TestClient
+from main import app
+from db import MyIngredientIn, MyIngredientOut
+from queries import MyIngredientQueries
 
-# client = TestClient(app)
-
-
-# def test_get_myinventory():
-#     user_id = 1
-
-#     response = client.get(f"/api/myingredients/?user_id={user_id}")
-
-#     assert response.status_code == 200
-#     response_data = response.json()
-#     assert isinstance(response_data, list)
+client = TestClient(app)
 
 
-# def test_add_to_myinventory():
-#     info = MyIngredientIn(
-#         user_id=1,
-#         ingredient_name="Pancakes",
-#         measurement_id=1,
-#         measurement_qty_id=2,
-#         notes="Some notes",
-#     )
+class EmptyIngredientQueries:
+    def get_all_my_ingredients(self, user_id):
+        return []
 
-#     response = client.post("/api/myingredients/", json=info.dict())
+    def get_my_ingredient_list(self, ingredient):
+        # Create new ingredient based on the input data
+        return []
 
-#     assert response.status_code == 200
-#     assert response.json()["ingredient_name"] == "Pancakes"
+    def get_my_ingredient_by_id(self, ingredient_id):
+        # Retrieve ingredient based on the given ID
+        return None
 
 
-# def test_update_myinventory():
-#     ingredient_id = 1
-#     info = {
-#         "user_id": 1,
-#         "ingredient_name": "Updated Pancakes",
-#         "measurement_id": 2,
-#         "measurement_qty_id": 3,
-#         "notes": "updated notes",
-#     }
+class TwoIngredientQueries:
+    def get_all_my_ingredients(self, user_id):
+        return [
+            MyIngredientOut(
+                id=1,
+                user_id=1,
+                ingredient_name="Pancakes",
+                measurement_id=1,
+                measurement_qty_id=2,
+                notes="Some notes",
+            ),
+            MyIngredientOut(
+                id=2,
+                user_id=1,
+                ingredient_name="Strawberry Pancakes",
+                measurement_id=1,
+                measurement_qty_id=2,
+                notes="Some notes",
+            ),
+        ]
 
-#     response = client.put(f"/api/myingredients/{ingredient_id}", json=info)
+    def get_my_ingredient_by_id(self, item_id):
+        return MyIngredientQueries(
+            id=1,
+            user_id=1,
+            ingredient_name="Pancakes",
+            measurement_id=1,
+            measurement_qty_id=2,
+            notes="Some notes",
+        )
 
-#     assert response.status_code == 200
-#     assert response.json()["ingredient_name"] == "Updated Pancakes"
+
+def test_add_to_myingredients():
+    app.dependency_overrides[MyIngredientQueries] = MyIngredientQueries
+    info = MyIngredientIn(
+        id=1,
+        user_id=1,
+        ingredient_name="Pancakes",
+        measurement_id=1,
+        measurement_qty_id=2,
+        notes="Some notes",
+    )
+
+    response = client.post("/api/myingredients/", json=info.dict())
+
+    assert response.status_code == 200
+    assert response.json()["ingredient_name"] == "Pancakes"
+
+    app.dependency_overrides[MyIngredientQueries] = MyIngredientQueries
 
 
-# def test_get_myinventory_list_ingredient():
-#     ingredient_id = 1
+def test_update_myingredient():
+    app.dependency_overrides[MyIngredientQueries] = MyIngredientQueries
+    ingredient_id = 1
+    info = MyIngredientIn(
+        id=1,
+        user_id=1,
+        ingredient_name="Updated Pancakes",
+        measurement_id=2,
+        measurement_qty_id=3,
+        notes="Updated notes",
+    )
 
-#     response = client.get(f"/api/myingredients/{ingredient_id}")
+    response = client.put(
+        f"/api/myingredients/{ingredient_id}", json=info.dict()
+    )
 
-#     assert response.status_code == 200
-#     assert response.json()["id"] == ingredient_id
+    assert response.status_code == 200
+    assert response.json()["ingredient_name"] == "Updated Pancakes"
+
+    app.dependency_overrides[MyIngredientQueries] = MyIngredientQueries
